46a20b81797b4fbe8cfd96b5af925bb41497eada
Doye7 <cdoye7@gmail.com>
2017-02-07 15:15:07 +0000

    Basic person class
    Allows creation of a person

---
+++ Code/INSEBase/src/insebase/Person.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package insebase;
+
+/**
+ *
+ * @author Doye
+ */
+
+// Person class
+public class Person {
+    private String fullName, shortName;
+    private int maxHours, idNum;
+    
+    // For creation of a new person
+    public Person(String name, String initials, int hours, int ID){
+        fullName = name;
+        shortName = initials;
+        maxHours = hours;
+        idNum = ID;
+    }
+    
+    // Get methods, just returns the value
+    public String getFullName(){
+        return fullName;
+    }
+    public String getShortName(){
+        return shortName;
+    }
+    public int getMaxHours(){
+        return maxHours;               
+    }
+    public int getIdNumber(){
+        return idNum;
+    }
+    
+    //Set methods, changes the value
+    public void setFullName(String newName){
+        fullName = newName;
+    }
+    public void setShortName(String newShortName){
+        shortName = newShortName;
+    }
+    public void setMaxHours(int newMaxHours){
+        maxHours = newMaxHours;
+    }
+    public void setIdNumber(int newId){
+        idNum = newId;
+    }
+}
